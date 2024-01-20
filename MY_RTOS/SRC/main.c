e4a47012d5c61c39e651804b25a3a3ffc1cd4315
Hjw-Arch <hujinwei121@163.com>
2024-01-20 18:45:45 +0800

    update

--- MY_RTOS/SRC/main.c
+++ MY_RTOS/SRC/main.c
@@ -2,6 +2,8 @@
 #include "ARMCM3.h"
 
 
+task_t* idleTaskp;
+
 void triggerPenderSVC (void) {
 	MEM8(NVIC_SYSPRI2) = NVIC_PENDSV_PRI;
 	MEM32(NVIC_INT_CTRL) = NVIC_PENDSVSET;
@@ -31,19 +33,48 @@ void taskInit (task_t* task, void (*entry)(void*), void* param, taskStack_t* sta
 	
 		
 	task->stack = stack;
+	
+	task->delayTicks = 0;
 }
 
 void taskSched(void) {
-	if (currentTask == *taskTable) {
-		nextTask = *(taskTable + 1);
-	} else {
-		nextTask = *taskTable;
+	if (currentTask == idleTaskp) {
+		if (taskTable[0]->delayTicks == 0) nextTask = taskTable[0];
+		else if (taskTable[1]->delayTicks == 0) nextTask = taskTable[1];
+		else return;
+	}
+	
+	if (currentTask == taskTable[0] && taskTable[1]->delayTicks == 0) {
+		nextTask = taskTable[1];
+	} else if (currentTask == taskTable[1] && taskTable[0]->delayTicks == 0){
+		nextTask = taskTable[0];
+	} else if (taskTable[0]->delayTicks != 0 && taskTable[1] != 0) {
+		nextTask = idleTaskp;
 	}
 	
 	taskSwitch();
 }
 
-// 设置SysTick定时时间
+void taskTimeSliceHandler() {
+	for (int i = 0; i < 2; ++i) {
+		if (taskTable[i]->delayTicks > 0) {
+			taskTable[i]->delayTicks--;
+		}
+	}
+	
+	taskSched();
+}
+
+void taskDelay (uint32_t ms) {
+	if (ms < TIME_SLICE) ms = TIME_SLICE;
+	
+	currentTask->delayTicks = (ms + TIME_SLICE / 2) / TIME_SLICE; // 四舍五入算法
+	
+	taskSched();
+}
+
+// 设置SysTick定时中断的时间（中断周期，任务的时间片）
+// 
 void setSysTick(uint32_t ms) {
 	SysTick->LOAD = ms * SystemCoreClock / 1000 - 1; // 设定预载值
 	NVIC_SetPriority(SysTick_IRQn, (1 << __NVIC_PRIO_BITS) - 1);  //设定优先级
@@ -52,28 +83,27 @@ void setSysTick(uint32_t ms) {
 }
 
 void SysTick_Handler() {
-	taskSched();
+	taskTimeSliceHandler();
 }
 
+// 要实现任务延时，需要使用定时器，而且每个任务都配备一个定时器才行，但是硬件只有一个定时器而任务数量很多
+// 因此可以利用SysTick产生这个硬件定时器来实现软定时器
+// 因为SysTick周期性触发中断，因此可以以这个周期为最基本的软件定时器的时间单位
+// 每触发一次SysTick就将软定时器的值-1即可
+// 因此软定时器的定时时间都是SysTick中断的倍数
+// 中断处理也需要时间，不能太频繁这样会导致系统在切换任务这个事情上占用太多资源，本末倒置，一般设置为10ms-100ms就可以
 
-void delay(int count) {
-	while (--count) ;
-}
 
-task_t ttask1;
-task_t ttask2;
-
-taskStack_t task1Env[1024];
-taskStack_t task2Env[1024];
+// 软定时器延时精度并不准确，要注意使用场合 eg：延时一个单位，在两个SysTick中间开始延时，那么只能延时半个SysTick中断周期。
+// 假如在将要触发定时器中断的时候发生了更高级别的中断，会导致延时时间变长
 
 int task1Flag;
 void task1Entry (void* param) {
-	setSysTick(10);
 	while(1) {
 		task1Flag = 0;
-		delay(100);
+		taskDelay(20);
 		task1Flag = 1;
-		delay(100);
+		taskDelay(20);
 	}
 }
 
@@ -81,21 +111,38 @@ int task2Flag;
 void task2Entry (void* param) {
 	while(1) {
 		task2Flag = 0;
-		delay(100);
+		taskDelay(20);
 		task2Flag = 1;
-		delay(100);
+		taskDelay(20);
 	}
 }
 
+task_t ttask1;
+task_t ttask2;
+task_t idleTask;
+
+taskStack_t task1Env[1024];
+taskStack_t task2Env[1024];
+taskStack_t idleTaskEnv[512];
+
+
+void idleTaskEntry (void* param) {
+	while(1) ;
+}
+
 int main(){
-	
+	setSysTick(TIME_SLICE);
 	taskInit(&ttask1, task1Entry, (void*)0x1145, &task1Env[1024]);
 	taskInit(&ttask2, task2Entry, (void*)0x1919, &task2Env[1024]);
+	
+	// idletask
+	taskInit(&idleTask, idleTaskEntry, (void*)0, &idleTaskEnv[512]);
 	
 	taskTable[0] = &ttask1;
 	taskTable[1] = &ttask2;
 	
 	nextTask = &ttask1;
+	idleTaskp = &idleTask;
 	
 	runFirstTask(); // 运行之后不会返回，下方的return 0其实没什么作用
 	
